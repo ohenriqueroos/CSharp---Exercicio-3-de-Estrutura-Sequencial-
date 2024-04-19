16b2c1ab3c9a7c3e7fee6918a4fe65ca3ac0f0dc
henri <henri@HENRIQUE>
2024-04-18 23:05:37 -0300

    Add project files.

---
+++ SequentialStructureExercise3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SequentialExercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+
+            int diff = (a * b) - (c * d);
+
+            Console.WriteLine(diff);
+        }
+    }
+}
